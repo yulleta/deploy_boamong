41e556b83c57ac4d35c0db4cccde4450ca0f90d3
KIM MINKYUNG <81565724+yulleta@users.noreply.github.com>
2023-12-05 17:20:24 +0900

    Add files via upload

--- ppl_plot.py
+++ ppl_plot.py
@@ -9,8 +9,21 @@ import numpy as np
 
 # Firebase 초기화 체크 및 실행
 if not firebase_admin._apps:
+    firebase_config = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    }
+    
     # cred = credentials.Certificate("./boamong-25709-firebase-adminsdk-szy07-2a5ec92369.json")
-    cred = credentials.Certificate(st.secrets['json'])
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 # Firestore 인스턴스 생성
 db = firestore.client()
